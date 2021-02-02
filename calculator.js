feccedee384717bb7d4e61157a30c13fe143a011
Mia Bueno <mia.bueno101@gmail.com>
2021-02-02 23:12:51 +1100

    console log showing values submitted through form on body

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 var port = 3000;
 
@@ -8,6 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', (req, res) => {
+    console.log(req.body);
     res.send("Thanks for post");
 });
 
