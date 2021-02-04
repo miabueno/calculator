21abfaa3ecb487c1082a08ede37ba4f1fcef11bd
Mia Bueno <mia.bueno101@gmail.com>
2021-02-04 11:48:17 +1100

    add routes for simple BMI calculator

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 var port = 3000;
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', (req, res) => {
@@ -14,6 +14,17 @@ app.post('/', (req, res) => {
     var num2 = Number(req.body.num2);
     sum = num1 + num2;
     res.send("Addition of this is " + sum);
+});
+
+app.get('/bmicalculator', (req, res) => {
+    res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmicalculator', (req, res) => {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight/(height ** 2);
+    res.send("<h1>Bmi is " + bmi + "</h1>");
 });
 
 app.listen(port, (req, res) => {
